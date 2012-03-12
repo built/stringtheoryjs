0dfcae9b0d59b03bf2d4dea4cbe04cb7ff26026f
Matt Youell <matt@youell.com>
2012-03-11 21:59:12 -0700

    Migrated tests to QUnit

--- stringtheory_tests.js
+++ stringtheory_tests.js
@@ -1,116 +1,103 @@
-// Test suite for StringTheory for JS v0.01. Yay.
+// Test suite for StringTheory for JS v0.01.
+// Now using QUnit.
+$(document).ready(function(){
 
-function testResult(condition)
-{
-	if(condition) return "<span class='pass'>PASSED</span>";
-	
-	return "<span class='fail'>FAILED</span>";
-}
+    var str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
 
-function showTest(testName, test)
-{
-	document.write(testResult(test) + "&nbsp;&nbsp;&nbsp;" + testName + "<br/>");
-}
+    module("StringTheory Core");
 
+    test("Initialization test 1", function() {
+      equal( str.length(), 58, "Length should be 58." );
+    });
 
-var str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
+    test("Copy tests", function() {
+        equal(str.copy(0, 3), "Mar");
+        equal(str.copy(55, 3), "vy.");
+        equal(str.copy(55, 10), "vy."); // Over-runs should be handled too.
+        equal(str.copy(58, 3), "Mar"); // Out-of-bounds indices should be modulated.
+        equal(str.copy(0, 0), ""); // Don't freak out if asked for nothing.
+        equal(str.copy(), "Mares eat oats and does eat oats but little lambs eat ivy."); // Copy the entire string if no params.
+    });
 
-try
-{
+    test("Cut tests", function() {
+        equal(str.cut(0, 3), "Mar");
+        equal(str, "es eat oats and does eat oats but little lambs eat ivy.");
 
-showTest("Initialization test 1", str.length = 58);
+        str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
 
-showTest("Copy Test 1", (str.copy(0, 3) == "Mar") );
-showTest("Copy Test 2", (str.copy(55, 3) == "vy.") );
-showTest("Copy Test 3", (str.copy(55, 10) == "vy.") ); // Over-runs should be handled too.
-showTest("Copy Test 4", (str.copy(58, 3) == "Mar") ); // Out-of-bounds indices should be modulated.
-showTest("Copy Test 5", (str.copy(0, 0) == "") ); // Don't freak out if asked for nothing.
+        equal(str.cut(55, 3), "vy.");
+        equal(str, "Mares eat oats and does eat oats but little lambs eat i");
+    });
 
-showTest("Copy Test 6", (str.copy() == "Mares eat oats and does eat oats but little lambs eat ivy.") ); // Copy the entire string if no params.
+    result = "Mares eat oats and does eat oats but little lambs eat ivy.";
 
+    test("Paste tests", function() {
+        str = new ST("eat oats and does eat oats but little lambs eat ivy.");
+        str.paste(0, "Mares ");
+        equal(str.text, result);
 
-showTest("Cut Test 1", (str.cut(0, 3) == "Mar") );
-showTest("Cut Test 2", (str == "es eat oats and does eat oats but little lambs eat ivy.") );
+        str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
+        str.paste(0, "");
+        equal(str.text, result);
 
-str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
-showTest("Cut Test 3", (str.cut(55, 3) == "vy.") );
-showTest("Cut Test 4", (str == "Mares eat oats and does eat oats but little lambs eat i") );
+        str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
+        str.paste(str.text.length - 1, "");
+        equal(str.text, result);
+    });
 
-result = "Mares eat oats and does eat oats but little lambs eat ivy.";
+    test("Append/Prepend", function() {
+        str = new ST("Mares eat oats");
+        str.append(" and does eat oats but little lambs eat ivy.");
 
-str = new ST("eat oats and does eat oats but little lambs eat ivy.");
-str.paste(0, "Mares ");
-showTest("Paste Test 1", str.text == result);
+        equal(str.text, result);
 
-str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
-str.paste(0, "");
-showTest("Paste Test 2", str.text == result);
+        str = new ST(" and does eat oats but little lambs eat ivy.");
+        str.prepend("Mares eat oats");
 
-str = new ST("Mares eat oats and does eat oats but little lambs eat ivy.");
-str.paste(str.text.length - 1, "");
-showTest("Paste Test 3", str.text == result);
-
-str = new ST("Mares eat oats");
-str.append(" and does eat oats but little lambs eat ivy.");
-
-showTest("Append Test 1", str.text == result);
-
-
-str = new ST(" and does eat oats but little lambs eat ivy.");
-str.prepend("Mares eat oats");
-
-showTest("Prepend Test 1", str.text == result);
+        equal(str.text, result);
+    });
 
 
-str = new ST("Mares eat oats");
-str.quote();
+    test("Quote tests", function() {
+    str = new ST("Mares eat oats");
+    str.quote();
 
-showTest("Quote Test 1", str.text == "\"Mares eat oats\"");
+    equal(str.text, "\"Mares eat oats\"");
 
-str = new ST("Mares eat oats");
-str.quote('xXx');
+    str = new ST("Mares eat oats");
+    str.quote('xXx');
 
-showTest("Quote Test 2", str.text == "xXxMares eat oatsxXx");
+    equal(str.text, "xXxMares eat oatsxXx");
+    });
 
-str = new ST("Mares eat oats");
-showTest("Word Count Test 1", str.words().length == 3 );
+    test("Word tests", function() {
+    str = new ST("Mares eat oats");
+    equal(str.words().length, 3);
 
-str = new ST("");
-showTest("Word Count Test 2", str.words().length == 0 );
+    str = new ST("");
+    equal(str.words().length, 0);
 
-str = new ST("This\tis\ta\tzither");
-showTest("Word Count Test 3", str.words().length == 4 );
+    str = new ST("This\tis\ta\tzither");
+    equal(str.words().length, 4);
+    });
 
+    test("Ellipsize tests", function() {
+    str = new ST("Mares eat oats");
+    str.ellipsize(12);
+    equal(str, "Mares eat...");
+    });
 
-str = new ST("Mares eat oats");
-str.ellipsize(12);
-showTest("Ellipsize Test 1", str == "Mares eat...");
+    test("Populate tests", function() {
+    str = new ST("Mares eat %GRAIN% and does eat %GRAIN% but little lambs eat %PLANT%.");
 
-str = new ST("Mares eat %GRAIN% and does eat %GRAIN% but little lambs eat %PLANT%.");
+    var translate = {};
 
-var translate = {};
+    translate["GRAIN"] = "oats";
+    translate["PLANT"] = "ivy";
 
-translate["GRAIN"] = "oats";
-translate["PLANT"] = "ivy";
+    str.populate(translate);
+    equal(str, "Mares eat oats and does eat oats but little lambs eat ivy.");
+    });
 
-str.populate(translate);
-showTest("Populate Test 1", str == "Mares eat oats and does eat oats but little lambs eat ivy.");
-
-
-document.write(str);
-
-}
-catch(ex)
-{
-	showTest("<div class='exception'>SCRIPT ERROR. " + ex + "</div>", false);
-	document.write();
-}
-
-
-
-
-
-
-
-
+});
 
